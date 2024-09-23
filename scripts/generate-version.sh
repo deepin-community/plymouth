23857ae8cc988fcf511d7ca28a0dff551e2f6e1e
Mingcong Bai <baimingcong@uniontech.com>
2024-09-23 15:22:23 +0800

    fix: fix NVIDIA Optimus freeze; do not show Debian watermark
    Backport two upstream patches to fix splash freezes on framebuffer and
    non-modeset devices (i.e., NVIDI Optimus laptops). This was addressed by
    the upstream in the following pull request:
    
    https://gitlab.freedesktop.org/plymouth/plymouth/-/merge_requests/334.
    
    Credit goes to Kexy Biscuit <kexybiscuit@aosc.io> for bisecting and
    backporting these patches. Tested good on AOSC OS and should apply for
    deepin/UOS as well. See original pull request:
    
    https://github.com/AOSC-Dev/aosc-os-abbs/pull/7864
    
    With regards to the Debian watermark (originally part of 0.9.5.1-deepin3):
    
    Debian's Plymouth hook installs a Debian watermark on the right side of
    the display if:
    
    * The user has installed desktop-base (a very common practice for Debian
      desktop systems, but not for deepin/UOS, as we do not use tasksel).
    * The user has chosen a two-step-type Plymouth theme (which deepin-logo is
      one of that type).

--- scripts/generate-version.sh
+++ scripts/generate-version.sh
@@ -5,20 +5,6 @@ cd "$SRCDIR"
 CWD=$(realpath "$PWD")
 exec 2>&3
 
-# If it's not from a git checkout, assume it's from a tarball
-if ! git rev-parse --is-inside-git-dir > /dev/null 2>&1; then
-    VERSION_FROM_DIR_NAME=$(basename "$CWD" | sed -n 's/^plymouth-\([^-]*\)$/\1/p')
-
-    if [ -n "$VERSION_FROM_DIR_NAME" ]; then
-        echo "$VERSION_FROM_DIR_NAME"
-        exit 0
-    fi
-
-    echo "Source doesn't appear to come from an plymouth git clone or tarball. Version unknown."
-    exit 1
-fi
-
-# If it is from a git checkout, derive the version from the date of the last commit, and the number
-# of commits since the last release.
-COMMITS_SINCE_LAST_RELEASE=$(git rev-list $(git describe --abbrev=0)..HEAD --count)
-date +%y.%j.${COMMITS_SINCE_LAST_RELEASE} -d "@$(git log -1 --pretty=format:%ct)"
+head -n1 "$SRCDIR"/debian/changelog | \
+	cut -f2 -d'(' | \
+	cut -f1 -d')'
